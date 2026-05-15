a39bee81b565f58148499b48b60a2c83838ab1a6
holipay <nase@live.com>
2026-05-15 15:49:37 +0800

    fix: P0+P1 - RelayCommand<T>, Toast timer leak, Cat1-Cat2 linking, filter ItemsSource

--- Cashlane/ViewModels/RelayCommand.cs
+++ Cashlane/ViewModels/RelayCommand.cs
@@ -27,3 +27,32 @@ public class RelayCommand : ICommand
     public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
     public void Execute(object? parameter) => _execute(parameter);
 }
+
+public class RelayCommand<T> : ICommand
+{
+    private readonly Action<T?> _execute;
+    private readonly Func<T?, bool>? _canExecute;
+
+    public RelayCommand(Action<T?> execute, Func<T?, bool>? canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public bool CanExecute(object? parameter) =>
+        _canExecute?.Invoke(parameter is T t ? t : default) ?? true;
+
+    public void Execute(object? parameter)
+    {
+        if (parameter is T t)
+            _execute(t);
+        else
+            _execute(default);
+    }
+}
