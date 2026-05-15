bbf8e27380b3e479f2575c129e959f46ca74dfb9
holipay <nase@live.com>
2026-05-15 16:13:17 +0800

    feat: Excel import/export with ClosedXML
    - Add ClosedXML NuGet package
    - Create ExcelService: export expenses to .xlsx, import from .xlsx
    - Wire up ImportCommand with file open dialog and name-to-ID resolution
    - Wire up ExportCommand with file save dialog
    - Register ExcelService in DI container

--- Cashlane/Views/ExpenseTableView.xaml.cs
+++ Cashlane/Views/ExpenseTableView.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using Cashlane.Models;
 using Cashlane.ViewModels;
+using Microsoft.Win32;
 
 namespace Cashlane.Views;
 
@@ -22,6 +23,29 @@ public partial class ExpenseTableView : UserControl
             {
                 var result = MessageBox.Show(message, title, MessageBoxButton.YesNo, MessageBoxImage.Warning);
                 return result == MessageBoxResult.Yes;
+            };
+
+            vm.ShowOpenFileDialog = () =>
+            {
+                var dlg = new OpenFileDialog
+                {
+                    Title = "选择要导入的 Excel 文件",
+                    Filter = "Excel 文件|*.xlsx;*.xls",
+                    FilterIndex = 1
+                };
+                return dlg.ShowDialog() == true ? dlg.FileName : null;
+            };
+
+            vm.ShowSaveFileDialog = () =>
+            {
+                var dlg = new SaveFileDialog
+                {
+                    Title = "导出费用记录",
+                    Filter = "Excel 文件|*.xlsx",
+                    FileName = $"费用记录_{DateTime.Now:yyyyMMdd}",
+                    DefaultExt = ".xlsx"
+                };
+                return dlg.ShowDialog() == true ? dlg.FileName : null;
             };
         }
     }
